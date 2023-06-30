56ff6ae1e000c0ecbb3fcf5d9332655e5a02344c
metus01 <carnelmetus@gmail.com>
2023-06-30 12:15:32 +0100

    ajout de la fonctionalité de recherche

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\User;
 use Livewire\Component;
 
 class UsersTable extends Component
 {
+    // public $users = [];
+    public string $search  = '';
     public function render()
     {
-        return view('livewire.users-table');
+        return view('livewire.users-table' ,
+    [
+        'users' => User::
+        where('name', 'LIKE', $this->search)->get(),
+    ]);
     }
 }
--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -9,42 +9,7 @@
     @livewireStyles
 </head>
 <body>
-    <nav class="navbar bg-light">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="#">Navbar</a>
-        </div>
-      </nav>
-      <div class="container">
-     
-        <input  class=" mt-4 mb-4 form-control" placeholder="Rechercher" type="text">
-      </div>
-        <div class=" mt-4 mb-4 container table-responsive">
-            <table class="table table-striped">
-                <thead>
-                    <tr>
-                        <th class="text text-center">Name</th>
-                        <th class="text text-center">Title</th>
-                        <th>Statut</th>
-                        <th>Rôle</th>
-                        <th class="text text-end">Actions</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($users as $user)
-                    <tr>
-                        <td class="text text-center">{{ $user->name }}</td>
-                        <td class="text text-center">{{ $user->title }}</td>
-                        <td class="text text-center">{{ $user->online }}</td>
-                        <td class="text text-center">{{ $user->role }}</td>
-                        <td>
-                            <button class="btn btn-primary">Editer</button>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
+ <livewire:users-table></livewire:users-table>
     @livewireScripts
 <script src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
 </body>
