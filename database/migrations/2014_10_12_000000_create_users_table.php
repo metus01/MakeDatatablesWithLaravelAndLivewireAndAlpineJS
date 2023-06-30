d239afd7b428bb5796b893bb09baa93062175ba0
metus01 <carnelmetus@gmail.com>
2023-06-30 10:21:03 +0100

    modification de la migration sur les users

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role')->default('Admin');
+            $table->string('title');
+            $table->boolean('online')->default(true);
             $table->rememberToken();
             $table->timestamps();
         });
